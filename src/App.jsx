bfca233175c8f8229bc90c8f9abf5f936691080c
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-02 16:12:17 +0500

    TMDB API Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { fetchDataFromApi } from "./utils/api"
+
 function App() {
+  useEffect(() => {
+    apiTesting()
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then(res => {
+        console.log(res)
+      });
+  }
+
   return (
     <>
       <div>
