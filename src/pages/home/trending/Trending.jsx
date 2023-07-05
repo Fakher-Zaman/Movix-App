14e852f64d027e526071c1672ed210e5d99dad4c
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-05 15:33:21 +0500

    Carousel Section - Main Carousel Component

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
 import SwitchTab from "../../../components/switchTabs/SwitchTab"
+import Carousel from "../../../components/carousel/Carousel"
 
 import useFetch from "../../../hooks/useFetch"
 
@@ -20,6 +21,7 @@ const Trending = () => {
                 <span className="carouselTitle">Trending</span>
                 <SwitchTab data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     )
 }
