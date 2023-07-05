0c106e798f117c9ae813911b50b606196b334158
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-05 16:46:58 +0500

    Genres Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { fetchDataFromApi } from "./utils/api"
 import { useDispatch, useSelector } from "react-redux"
-import { getApiConfiguration } from "./store/homeSlice"
+import { getApiConfiguration, getGenres } from "./store/homeSlice"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/home/Home"
@@ -18,6 +18,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig()
+    genresCall()
   }, []);
 
   const fetchApiConfig = () => {
@@ -33,6 +34,24 @@ function App() {
 
         dispatch(getApiConfiguration(url))
       });
+  }
+
+  const genresCall = async () => {
+    let promises = []
+    let endPoints = ["tv", "movie"]
+    let allGenres = {}
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    })
+
+    const data = await Promise.all(promises)
+    // console.log(data)
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item))
+    })
+    // console.log(allGenres)
+    dispatch(getGenres(allGenres))
   }
 
   return (
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -11,6 +11,7 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 import "./style.scss"
 
@@ -48,6 +49,7 @@ const Carousel = ({ data, loading }) => {
                                     <div className="posterBlock">
                                         <Img src={posterUrl} />
                                         <CircleRating rating={item.vote_average.toFixed(1)} />
+                                        <Genres data={item.genre_ids.slice(0, 2)} />
                                     </div>
                                     <div className="textBlock">
                                         <span className="title">
