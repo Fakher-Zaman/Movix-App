d05236170a928406c2265012377d49453910653b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-05 11:45:27 +0500

    Carousel Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import HeroBanner from "./heroBanner/HeroBanner"
+import Trending from "./trending/Trending"
 import "./style.scss"
 
 const Home = () => {
     return (
         <div className="homePage">
             <HeroBanner />
+            <Trending />
             <div style={{ height: 1000 }}></div>
         </div>
     )
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,13 @@
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
+
+const Trending = () => {
+    return (
+        <div className="carouselSection">
+            <ContentWrapper className="contentWrapper">
+                <span className="carouselTitle">Trending</span>
+            </ContentWrapper>
+        </div>
+    )
+}
+
+export default Trending
