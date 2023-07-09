db0215f729b7e7659b039dc2f5f3e943ef2ee461
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-09 20:59:30 +0500

    Details Page - Videos Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,16 +5,20 @@ import "./style.scss"
 
 import DetailsBanner from "./detailsBanner.jsx/DetailsBanner"
 import Cast from "./cast/Cast"
+import VideosSection from "./videosSection/VideosSection"
 
 const Details = () => {
-    const { mediaType, id } = useParams()
-    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
-    const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/videos`)
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
 
     return (
         <div>
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
             <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={loading} />
         </div>
     )
 }
